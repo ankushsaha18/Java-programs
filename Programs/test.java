4afee454395dbef7072793e8829ddd79441b551a
Ankush Saha <ankushsaha806@gmail.com>
2023-11-29 03:06:31 +0530

    second change

--- Programs/test.java
+++ Programs/test.java
@@ -1,18 +1,28 @@
 import java.util.*;
-class test{
-    public static void main(String[] args) {
-       Scanner sc=new Scanner(System.in);
-       int n=sc.nextInt();
-       int a=0;
-       int b=1;
-       int count=2;
-       while(count<=n){
-        int c=a+b;
-        a=b;
-        b=c;
-        count++;
-       }
-       System.out.println(b);
+public class test{
+   public static void main(String[] args) {
+      int[] nums={1,2,3,4,5,6,7};
+   int k=3;
+   reverse(nums,k);
+   }
+   
+   static void rev(int[] nums,int start,int end){
+      ArrayList<Integer> ar2=new ArrayList<>();
+     for(int i=end;i>=start;i--){
+        ar2.add(nums[i]);
+     }
 
+     for(int i=0;i<ar2.size();i++){
+        nums[start+i]=ar2.get(i);
+     }
+   }
+
+    public static void reverse(int nums[], int k){
+      System.out.println(Arrays.toString(nums));
+    rev(nums,0,nums.length-1);
+    rev(nums,0,k-1);
+    rev(nums,k,nums.length-1);
+    System.out.println(Arrays.toString(nums));
+     
     }
-}
+  }
