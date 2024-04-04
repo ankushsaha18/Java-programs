31951d38c8717699ffa41a58fdd8d0eea3293fc1
Ankush Saha <ankushsaha806@gmail.com>
2024-04-04 15:10:46 +0530

    modified

--- Programs/hlp.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class hlp {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        String a=sc.next();
-        System.out.print(a);
-    }
-    
-}
--- Programs/tempCodeRunnerFile.java
+++
@@ -1,5 +0,0 @@
-for (int i = 1; i < count; i++) {
-        // if (i != parr[i]) {
-        // return i;
-        // }
-        // }
