f00995db19a71a61e85e152e512983e951c3375e
Ankush Saha <ankushsaha806@gmail.com>
2024-09-01 23:16:19 +0530

    new added

--- Programs/Other/test.java
+++ Programs/Other/test.java
@@ -3,8 +3,23 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        String s = "Ankush";
-        System.out.println(s.substring(0,2));
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            int[] A = new int[N];
+            for (int i = 0; i < N; i++) {
+                A[i] = sc.nextInt();
+            }
+            Set<Integer> distinctElements = new HashSet<>();
+            for (int i = 0; i < N; i++) {
+                distinctElements.add(A[i]);
+            }
+            int numDistinct = distinctElements.size();
+            int largestK = N - (N % numDistinct);
+            System.out.println(largestK);
+        }
+        sc.close();
     }
 }
 
